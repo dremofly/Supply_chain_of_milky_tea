73dfe33ab926564fcb54e8b22d1e1052df419f16
dremofly <dremofly@gmail.com>
2019-06-22 22:51:52 +0800

    app的基本结构

---
+++ app/src/config/menu-config.js
@@ -0,0 +1,17 @@
+module.exports = [{
+  name: 'Supply Chain',
+  id: 'basic',
+  sub: [{
+    name: 'Farmer',
+    componentName: 'Farmer'
+  }, {
+    name: 'Distributor',
+    componentName: 'Distributor'
+  }, {
+    name: 'Retailer',
+    componentName: 'Retailer'
+  }, {
+    name: 'Consumer',
+    componentName: 'Consumer'
+  }]
+}]
