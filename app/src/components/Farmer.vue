c391bee849e31e9d7c8b8fa89b0a8dac625235e2
dremofly <dremofly@gmail.com>
2019-06-23 10:02:54 +0800

    往farmer.vue里面添加了界面

--- app/src/components/Farmer.vue
+++ app/src/components/Farmer.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    Farmer
+    <h1>Farmer Interface</h1>
+    UPC
+    <input class="input-field" type="number" id="sku" name="sku" value=1><br>
+    <br>
+    <button @click="goToVote()">Harvest</button>
+    <button>Process</button>
+    <button>Pack</button>
+    <button>Sell</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToVote () {
+      console.log('harvest')
+    }
+  }
+}
+</script>
